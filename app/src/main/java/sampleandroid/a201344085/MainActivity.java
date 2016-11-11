40e0e13ed52b8cae7be13dc9667e33748f9a0248
LBH <dlqudgml0503@naver.com>
2016-11-11 19:40:01 +0900

    캘린더에서 날짜를 찍으면 현재 날짜 2016-11-11 가 토스트로 나오게 만들기

--- app/src/main/java/sampleandroid/a201344085/MainActivity.java
+++ app/src/main/java/sampleandroid/a201344085/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RatingBar;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
     RatingBar MyRatingBar1;
     RadioButton rbtn1;
     RadioButton rbtn2;
+    CalendarView cal1;
+    long now = System.currentTimeMillis();
+    Date date = new Date(now);
+
+    SimpleDateFormat CurDateFormat = new SimpleDateFormat("yyyy년MM월dd일");
+/*    SimpleDateFormat CurYearFormat = new SimpleDateFormat("yyyy");
+    SimpleDateFormat CurMonthFormat = new SimpleDateFormat("mm");
+    SimpleDateFormat CurDayFormat = new SimpleDateFormat("dd");*/
+    String strCurDate = CurDateFormat.format(date);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         MyRatingBar1 = (RatingBar)findViewById(R.id.ratingBar);
         rbtn1 = (RadioButton)findViewById(R.id.radioButton);
         rbtn2 = (RadioButton)findViewById(R.id.radioButton2);
+        cal1 = (CalendarView)findViewById(R.id.calendarView);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,6 +91,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(rbtn2.isChecked()) Toast.makeText(getApplicationContext(), "여자입니다" ,Toast.LENGTH_SHORT).show();
                 else Toast.makeText(getApplicationContext(), "남자입니다",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        cal1.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                month = month+1;
+                 String now = year+"년"+month+"월"+dayOfMonth+"일";
+                if(strCurDate == now) Toast.makeText(getApplicationContext(), "오늘입니다",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(getApplicationContext(), year+"년"+month+"월"+dayOfMonth+"일 입니다",Toast.LENGTH_SHORT).show();
             }
         });
     }
