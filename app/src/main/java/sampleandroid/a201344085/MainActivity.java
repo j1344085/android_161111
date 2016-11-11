03b036fdfc6a0e83b5d7176afd298d60df0923a2
LBH <dlqudgml0503@naver.com>
2016-11-11 19:13:51 +0900

    레이팅바 이전까지 완료 커밋

--- app/src/main/java/sampleandroid/a201344085/MainActivity.java
+++ app/src/main/java/sampleandroid/a201344085/MainActivity.java
@@ -1,13 +1,49 @@
 package sampleandroid.a201344085;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn1;
+    CheckBox ck1;
+    int count1 = 0;
+    LinearLayout MyLayout1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = (Button)findViewById(R.id.button);
+        ck1 = (CheckBox)findViewById(R.id.checkBox);
+        MyLayout1 = (LinearLayout)findViewById(R.id.activity_main);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "Hello World",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ck1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = "";
+                if(ck1.isChecked()){
+                    str += ck1.getText() + " ";
+                    MyLayout1.setBackgroundColor(Color.rgb(0,0,255));
+                }
+                else MyLayout1.setBackgroundColor(Color.WHITE);
+
+                if(str.equals("")) Toast.makeText(getApplicationContext(), "선택이 해제되었습니다",Toast.LENGTH_SHORT).show();
+                else Toast.makeText(getApplicationContext(), str+"를 선택하였습니다",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
